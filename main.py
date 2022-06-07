ac31aa38fa178f13456eb8392982ace252e8d514
lagerat <maxluibarskiy@gmail.com>
2022-06-08 02:10:00 +0700

    Add simple table, and example for data

--- main.py
+++ main.py
@@ -1,2 +1,86 @@
+# from PyQt6.QtWidgets import  QApplication, QMainWindow
+# from PyQt6 import QtCore, QtGui, QtWidgets
+# from PyQt6.QtCore import Qt
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+
+
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.fisrtStageTable = QtWidgets.QTableView(self.centralwidget)
+        self.fisrtStageTable.setObjectName("fisrtStageTable")
+
+        ## Data for example
+        data = [
+            [4, 9, 2],
+            [1, 0, 0],
+            [3, 5, 0],
+            [3, 3, 2],
+            [7, 8, 9],
+        ]
+        self.tableModel = TableModel(data)
+        self.fisrtStageTable.setModel(self.tableModel)
+
+        self.verticalLayout.addWidget(self.fisrtStageTable)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.continueBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.continueBtn.setObjectName("continueBtn")
+        self.verticalLayout.addWidget(self.continueBtn)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "informLabel"))
+        self.continueBtn.setText(_translate("MainWindow", "Подтвердить"))
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
